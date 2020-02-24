7f341cb8da8c7287d089c63841576c2f1a370ebf
Larry Xue <larry.p.xue@gmail.com>
2020-02-24 17:40:45 -0500

    bugfix for gen file struct

--- sciolyid/scripts/generate_file_structure.py
+++ sciolyid/scripts/generate_file_structure.py
@@ -1,23 +1,22 @@
 import os
 
 def _groups():
-    """Converts txt files of data into lists."""
-    filenames = [name.strip(".txt") for name in os.listdir("PATH_TO/Fossil-ID/bot/data/lists")]
-    # Converts txt file of data into lists
-    lists = {}
-    for filename in filenames:
-        print(f"Working on {filename}")
-        with open(f'PATH_TO/Fossil-ID/bot/data/lists/{filename}.txt', 'r') as f:
-            lists[filename] = [line.strip().lower() for line in f]
-        print(f"Done with {filename}")
-    print("Done with lists!")
-    return lists
+	"""Converts txt files of data into lists."""
+	filenames = [name[:name.rfind(".")] for name in os.listdir("/bot/data/lists")]
+	# Converts txt file of data into lists
+	lists = {}
+	for filename in filenames:
+		print(f"Working on {filename}")
+		with open(f'/bot/data/lists/{filename}.txt', 'r') as f:
+			lists[filename] = [line.strip().lower() for line in f]
+		print(f"Done with {filename}")
+	print("Done with lists!")
+	return lists
 
 groups = _groups()
 
 for key in groups.keys():
-    for item in groups[key]:
-        os.makedirs(f"{key}/{item}")
-        with open(f"{key}/{item}/image.placeholder", "x") as f:
-            f.write("")
-
+	for item in groups[key]:
+		os.makedirs(f"{key}/{item}")
+		with open(f"{key}/{item}/image.placeholder", "x") as f:
+			f.write("")
