9bf16ddb59973ce7076791cb0ee8bd752b0ab295
Tomi Chen <tctree333@users.noreply.github.com>
2020-03-10 17:55:55 -0700

    add comments for config

--- sciolyid/config.py
+++ sciolyid/config.py
@@ -14,29 +14,30 @@ id_required = {
 
 optional = {
     "name": "id-bot",  # all lowercase, no spaces, doesn't really matter what this is
-    "download_dir": "github_download/",
-    "data_dir": "data/",
-    "list_dir": "lists/",
-    "restricted_list_dir": "restricted/",
-    "wikipedia_file": "wikipedia.txt",
-    "alias_file": "aliases.txt",
-    "logs": True,
-    "log_dir": "logs/",
-    "file_folder": "",
+    "download_func": None,  # asyncronous function that downloads images locally to download_dir
+    "download_dir": "github_download/",  # local directory containing media (images)
+    "data_dir": "data/",  # local directory containing the id data
+    "list_dir": "lists/",  # directory within data_dir containing id lists
+    "restricted_list_dir": "restricted/",  # directory within data_dir containg id lists that are avaliable by selection only
+    "wikipedia_file": "wikipedia.txt",  # filename within data_dir containing wiki urls for every item
+    "alias_file": "aliases.txt",  # filename within data_dir contiaining aliases for any item
+    "logs": True,  # enable logging
+    "log_dir": "logs/",  # directory for text logs/backups
+    "bot_files_dir": "",  # folder for bot generated files (downloaded images, logs)
     "short_id_type": "",  # short (usually 1 letter) form of id_type, used as alias for the pic command
     "invite": "This bot is currently not avaliable outside the support server.",  # bot server invite link
     "authors": "person_v1.32, hmmm, and EraserBird",  # creator names
     "id_groups": True,  # true/false - if you want to be able to select certain groups of items to id
     "category_aliases": {},  # aliases for categories
-    "disable_extensions": [],
-    "custom_extensions": [],
-    "sentry": False,
-    "local_redis": True,
-    "bot_token_env": "token",
-    "sentry_dsn_env": "SENTRY_DISCORD_DSN",
-    "redis_env": "REDIS_URL",
-    "backups_channel": None,
-    "backups_dir": "backups/"
+    "disable_extensions": [],  # bot extensions to disable (media, check, skip, hint, score, sessions, race, other)
+    "custom_extensions": [],  # custom bot extensions to enable
+    "sentry": False,  # enable sentry.io error tracking
+    "local_redis": True,  # use a local redis server instead of a remote url
+    "bot_token_env": "token",  # name of environment variable containing the discord bot token
+    "sentry_dsn_env": "SENTRY_DISCORD_DSN",  # name of environment variable containing the sentry dsn
+    "redis_env": "REDIS_URL",  # name of environment variable containing the redis database url
+    "backups_channel": None,  # discord channel id to upload database backups (None/False to disable)
+    "backups_dir": "backups/"  # directory to put database backup files before uploading
 }
 
 options = {d: e for d, e in list(required.items()) + list(id_required.items()) + list(optional.items())}
