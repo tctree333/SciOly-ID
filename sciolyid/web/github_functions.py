45de88d3e0633f4dfec6cfd7506528adacf549ad
Tomi Chen <tctree333@users.noreply.github.com>
2020-07-29 20:13:38 -0700

    update text, get hashes from api

--- sciolyid/web/github_functions.py
+++ sciolyid/web/github_functions.py
@@ -10,42 +10,51 @@ import sciolyid.config as config
 from github import Github, InputGitTreeElement
 
 g = Github(os.environ[config.options["github_token_env"]])
-repo = g.get_repo(config.options["validation_repo_id"])
+
+get_id = lambda x: "/".join(x.split("/")[-2:]).split(".")[0]
+
+validation_repo = g.get_repo(get_id(config.options["validation_repo_id"]))
+image_repo = g.get_repo(get_id(config.options["github_image_repo_url"]))
 
 
 def add_images(images: list, path: str, user_id: int, username: str):
     new_branch_name = f"user-{user_id}t{int(time.time()-1577865600)}"
-    master_sha = repo.get_git_ref("heads/master").object.sha
-    new_ref = repo.create_git_ref(f"refs/heads/{new_branch_name}", master_sha)
+    master_sha = validation_repo.get_git_ref("heads/master").object.sha
+    new_ref = validation_repo.create_git_ref(
+        f"refs/heads/{new_branch_name}", master_sha
+    )
     for item in images:
         filename = item.split("/")[-1]
         with open(item, "rb") as f:
             contents = f.read()
-        repo.create_file(
+        validation_repo.create_file(
             path=f"{path}{filename}",
             message=f"add file {filename}",
             content=contents,
             branch=new_branch_name,
         )
-    pull = repo.create_pull(
-        title="New Pull Request",
+    pull = validation_repo.create_pull(
+        title=f"Add files: {username}",
         head=new_branch_name,
         base="master",
-        body="add files",
+        body=f"User:{username}\nID:{user_id}",
         draft=False,
     )
-    pull.merge(commit_title="add files", merge_method="squash")
+    pull.merge(commit_title=f"add files by {username}", merge_method="squash")
     new_ref.delete()
+    return
 
 
 def find_duplicates(image, distance: int = 5) -> list:
     if isinstance(image, str):
         image = Image.open(image)
     current_hash = imagehash.phash(image)
+
+    content = image_repo.get_contents("hashes.csv")
+    r = csv.reader(content.decoded_content.decode("utf-8").strip().split("\n"))
+
     matches = []
-    with open(config.options["download_dir"] + "hashes.csv", "r") as f:
-        r = csv.reader(f)
-        for filename, hash_value in r:
-            if current_hash - imagehash.hex_to_hash(hash_value) <= distance:
-                matches.append(config.options["base_image_url"] + filename.strip("./"))
+    for filename, hash_value in r:
+        if current_hash - imagehash.hex_to_hash(hash_value) <= distance:
+            matches.append(config.options["base_image_url"] + filename.strip("./"))
     return matches
