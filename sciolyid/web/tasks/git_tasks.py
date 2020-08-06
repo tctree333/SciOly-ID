79570199111d8590808ffe77670ef75ca5ac4383
Tomi Chen <tctree333@users.noreply.github.com>
2020-08-05 22:55:21 -0700

    add git task to push changes

--- sciolyid/web/tasks/git_tasks.py
+++ sciolyid/web/tasks/git_tasks.py
@@ -1,11 +1,16 @@
 import json
+import os
 import random
+import shutil
 import time
 from typing import Callable, Union
 
 from celery.utils.log import get_task_logger
 
-from sciolyid.web.git import verify_repo
+import sciolyid.config as config
+from sciolyid.data import get_category
+from sciolyid.web.functions.images import filename_lookup
+from sciolyid.web.git import image_repo, verify_repo
 from sciolyid.web.tasks import celery_app, database
 
 logger = get_task_logger(__name__)
@@ -34,31 +39,40 @@ GIT_PUSH_OPCODES = (
     "END",  # 2
     "BEGIN",  # 1
 )
+CHECK_INTERVAL = 10  # 60*15
+
+
+def _push_helper(repo, commit_message, progress=None):
+    repo.remote("origin").pull()
+    index = repo.index
+    index.add("*")
+    index.commit(commit_message)
+    push_result = repo.remote("origin").push(progress=progress)
+    if len(push_result) == 0:
+        return None
+    set_flags = []
+    for i, flag in enumerate(f"{push_result[0].flags:0>11b}"):
+        if int(flag):
+            set_flags.append(GIT_PUSHINFO_FLAGS[i])
+    logger.info(set_flags)
+    return set_flags
 
 
 @celery_app.task
 def push(commit_message: str, user_id: Union[int, str]):
     logger.info("pushing!")
-    index = verify_repo.index
-    index.add("*")
-    index.commit(commit_message)
-    push_result = verify_repo.remote("origin").push(progress=gen_progress(user_id))
-    if len(push_result) == 0:
+    result = _push_helper(verify_repo, commit_message, progress=gen_progress(user_id))
+    if result is None:
         database.hset(
             f"sciolyid.upload.status:{user_id}",
             mapping={"status": json.dumps(["FAIL"]), "end": int(time.time())},
         )
         logger.error("push operation failed completely!")
     else:
-        set_flags = []
-        for i, flag in enumerate(f"{push_result[0].flags:0>11b}"):
-            if int(flag):
-                set_flags.append(GIT_PUSHINFO_FLAGS[i])
         database.hset(
             f"sciolyid.upload.status:{user_id}",
-            mapping={"status": json.dumps(set_flags), "end": int(time.time())},
+            mapping={"status": json.dumps(result), "end": int(time.time())},
         )
-        logger.info(set_flags)
     database.delete(f"sciolyid.upload.save:{user_id}")
     database.expire(f"sciolyid.upload.status:{user_id}", 60)
 
@@ -90,3 +104,48 @@ def gen_progress(user_id: Union[int, str]) -> Callable:
             logger.info(data)
 
     return wrapped_progress
+
+
+@celery_app.task
+def move_images():
+    logger.info("checking for move")
+    root = os.path.abspath(
+        config.options["validation_local_dir"] + config.options["validation_repo_dir"]
+    )
+    lookup = filename_lookup(root)
+    delete = set(
+        map(
+            lambda x: x.decode("utf-8"),
+            database.zrangebyscore("sciolyid.verify.images:invalid", 3, "+inf")
+            + database.zrangebyscore("sciolyid.verify.images:duplicate", 3, "+inf"),
+        )
+    )
+    database.zremrangebyscore("sciolyid.verify.images:invalid", 3, "+inf")
+    database.zremrangebyscore("sciolyid.verify.images:duplicate", 3, "+inf")
+    for image in delete:
+        os.remove(lookup[image])
+
+    valid = set(
+        map(
+            lambda x: x.decode("utf-8"),
+            database.zrangebyscore("sciolyid.verify.images:valid", 3, "+inf"),
+        )
+    )
+    database.zremrangebyscore("sciolyid.verify.images:valid", 3, "+inf")
+    for image in valid:
+        if image in delete:
+            continue
+        path = lookup[image]
+        item = os.path.dirname(os.path.relpath(path, root))
+        category = get_category(item)
+        shutil.copy(path, os.path.join(image_repo.working_tree_dir, category))
+        os.remove(path)
+
+    verify_push = _push_helper(verify_repo, "Update through verification!")
+    image_push = _push_helper(image_repo, "Update through verification!")
+
+    for result in (("verify repo", verify_push), ("image repo", image_push)):
+        if result[1] is None:
+            logger.info(result[0] + " failed completely!")
+        else:
+            logger.info(result[0] + " push success!")
