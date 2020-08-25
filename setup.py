26b76b999d4f0139119e13fa45d1f689189e41c8
Tomi Chen <tctree333@users.noreply.github.com>
2020-08-24 20:53:42 -0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setuptools.setup(
     name="sciolyid",
-    version="0.5.0-alpha.4",
+    version="0.5.0-alpha.5",
     description="Create ID Discord bots for SciOly studying.",
     long_description=readme(),
     long_description_content_type="text/markdown",
