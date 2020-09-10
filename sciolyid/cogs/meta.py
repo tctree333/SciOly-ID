2943f9724ff0208e1bf43a1639a4d724eef22770
Tomi Chen <tctree333@users.noreply.github.com>
2020-09-10 09:45:47 -0700

    formatting

--- sciolyid/cogs/meta.py
+++ sciolyid/cogs/meta.py
@@ -169,7 +169,12 @@ class Meta(commands.Cog):
     # ban command - prevents certain users from using the bot
     @commands.command(help="- ban command", hidden=True)
     @commands.is_owner()
-    async def ban(self, ctx, *, user: typing.Optional[typing.Union[discord.Member, discord.User]] = None):
+    async def ban(
+        self,
+        ctx,
+        *,
+        user: typing.Optional[typing.Union[discord.Member, discord.User]] = None,
+    ):
         logger.info("command: ban")
         if user is None:
             logger.info("no args")
@@ -182,7 +187,12 @@ class Meta(commands.Cog):
     # unban command - prevents certain users from using the bot
     @commands.command(help="- unban command", hidden=True)
     @commands.is_owner()
-    async def unban(self, ctx, *, user: typing.Optional[typing.Union[discord.Member, discord.User]] = None):
+    async def unban(
+        self,
+        ctx,
+        *,
+        user: typing.Optional[typing.Union[discord.Member, discord.User]] = None,
+    ):
         logger.info("command: unban")
         if user is None:
             logger.info("no args")
@@ -195,16 +205,25 @@ class Meta(commands.Cog):
     # correct command - see how many times someone got a bird correct
     @commands.command(help="- see answered birds command", hidden=True)
     @commands.is_owner()
-    async def correct(self, ctx, *, user: typing.Optional[typing.Union[discord.Member, discord.User]] = None):
+    async def correct(
+        self,
+        ctx,
+        *,
+        user: typing.Optional[typing.Union[discord.Member, discord.User]] = None,
+    ):
         logger.info("command: correct")
         if user is None:
             logger.info("no args")
             await ctx.send("Invalid User!")
             return
         logger.info(f"user-id: {user.id}")
-        await send_leaderboard(ctx, f"Top Correct Birds ({user.name})", 1, database_key=f"correct.user:{user.id}", items_per_page=25)
-
-
+        await send_leaderboard(
+            ctx,
+            f"Top Correct Birds ({user.name})",
+            1,
+            database_key=f"correct.user:{user.id}",
+            items_per_page=25,
+        )
 
 
 def setup(bot):
