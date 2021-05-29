02ca8e62b09105049609e54b44329e5057c776a5
erasedbird <39782319+erasedbird@users.noreply.github.com>
2021-05-28 21:01:54 -0700

    Added new functionalities to hint.py
    Added last (last char), count (number of char), and all (return all three pieces of info)

--- sciolyid/cogs/hint.py
+++ sciolyid/cogs/hint.py
@@ -25,16 +25,32 @@ class Hint(commands.Cog):
         self.bot = bot
 
     # give hint
-    @commands.command(help="- Gives first letter of current image", aliases=["h"])
+    @commands.command(help="- Gives first letter of current image", usage="[count|c last|l all|a]", aliases=["h"])
     @commands.check(CustomCooldown(3.0, bucket=commands.BucketType.channel))
-    async def hint(self, ctx):
-        logger.info("command: hint")
+    async def hint(self, ctx, *args):
+        logger.info("command: hint" + str(len(args)))
 
         current_item = database.hget(f"channel:{ctx.channel.id}", "item").decode(
             "utf-8"
         )
         if current_item != "":  # check if there is item
-            await ctx.send(f"The first letter is {current_item[0]}")
+            if(len(args) == 0):
+                await ctx.send(f"The first letter is {current_item[0]}.")
+            elif (args[0] == "count" or args[0] == "c"):
+                await ctx.send(f"The answer has {str(len(current_item))} letters.")
+            elif (args[0] == "last" or args[0] == "l"):
+                await ctx.send(f"The last letter is {current_item[len(current_item)-1]}.")
+            elif (args[0] == "all" or args[0] == "a"):
+                blanks = len(current_item)-2
+                the_hint = "`"+current_item[0]
+                for i in range(1, len(current_item)-1):
+                    if(current_item[i]!= " "):
+                        the_hint += " _ "
+                    else:
+                        the_hint+="   "
+                await ctx.send(the_hint+ current_item[len(current_item)-1] + "`")
+            else:
+                await ctx.send(f"The first letter is {current_item[0]}.")
         else:
             await ctx.send("You need to ask for a image first!")
 
