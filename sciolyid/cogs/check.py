2278877558c4d25a568c8dac473ea6af31cd26ad
Tomi Chen <tctree333@users.noreply.github.com>
2021-06-24 21:20:57 -0700

    unindent

--- sciolyid/cogs/check.py
+++ sciolyid/cogs/check.py
@@ -47,98 +47,98 @@ class Check(commands.Cog):
         )
         if current_item == "":  # no image
             await ctx.send("You must ask for a image first!")
-        else:  # if there is a image, it checks answer
-            arg = arg.lower()
-            current_item = current_item.lower()
-            logger.info("current_item: " + current_item)
-            logger.info("arg: " + arg)
+            return
 
-            item_setup(ctx, current_item)
-            correct_list = (x.lower() for x in get_aliases(current_item))
+        # if there is a image, it checks answer
+        arg = arg.lower()
+        current_item = current_item.lower()
+        logger.info("current_item: " + current_item)
+        logger.info("arg: " + arg)
+
+        item_setup(ctx, current_item)
+        correct_list = (x.lower() for x in get_aliases(current_item))
+
+        if database.exists(f"race.data:{ctx.channel.id}"):
+            logger.info("race in session")
+            if database.hget(f"race.data:{ctx.channel.id}", "strict"):
+                logger.info("strict spelling")
+                correct = arg in correct_list
+            else:
+                logger.info("spelling leniency")
+                correct = better_spellcheck(arg, correct_list, alias_id_list)
+        else:
+            logger.info("no race")
+            if database.hget(f"session.data:{ctx.author.id}", "strict"):
+                logger.info("strict spelling")
+                correct = arg in correct_list
+            else:
+                logger.info("spelling leniency")
+                correct = better_spellcheck(arg, correct_list, alias_id_list)
+
+        if correct:
+            logger.info("correct")
+
+            database.hset(f"channel:{ctx.channel.id}", "item", "")
+            database.hset(f"channel:{ctx.channel.id}", "answered", "1")
+
+            session_increment(ctx, "correct", 1)
+            streak_increment(ctx, 1)
+            database.zincrby(
+                f"correct.user:{ctx.author.id}",
+                1,
+                string.capwords(str(current_item)),
+            )
+
+            await ctx.send(
+                f"Correct! Good job! The image was **{current_item}**."
+                if not database.exists(f"race.data:{ctx.channel.id}")
+                else f"**{ctx.author.mention}**, you are correct! The image was **{current_item}**."
+            )
+            url = get_wiki_url(ctx, current_item)
+            await ctx.send(
+                url
+                if not database.exists(f"race.data:{ctx.channel.id}")
+                else f"<{url}>"
+            )  # sends wiki page
+            score_increment(ctx, 1)
+            if database.exists(f"race.data:{ctx.channel.id}"):
+
+                limit = int(database.hget(f"race.data:{ctx.channel.id}", "limit"))
+                first = database.zrevrange(f"race.scores:{ctx.channel.id}", 0, 0, True)[
+                    0
+                ]
+                if int(first[1]) >= limit:
+                    logger.info("race ending")
+                    race = self.bot.get_cog("Race")
+                    await race.stop_race(ctx)
+                else:
+                    logger.info("auto sending next image")
+                    group, state, bw = database.hmget(
+                        f"race.data:{ctx.channel.id}", ["group", "state", "bw"]
+                    )
+                    media = self.bot.get_cog("Media")
+                    await media.send_pic(
+                        ctx,
+                        group.decode("utf-8"),
+                        state.decode("utf-8"),
+                        bw.decode("utf-8"),
+                    )
+
+        else:
+            logger.info("incorrect")
+
+            streak_increment(ctx, None)
+            session_increment(ctx, "incorrect", 1)
+            incorrect_increment(ctx, str(current_item), 1)
 
             if database.exists(f"race.data:{ctx.channel.id}"):
-                logger.info("race in session")
-                if database.hget(f"race.data:{ctx.channel.id}", "strict"):
-                    logger.info("strict spelling")
-                    correct = arg in correct_list
-                else:
-                    logger.info("spelling leniency")
-                    correct = better_spellcheck(arg, correct_list, alias_id_list)
+                await ctx.send("Sorry, that wasn't the right answer.")
             else:
-                logger.info("no race")
-                if database.hget(f"session.data:{ctx.author.id}", "strict"):
-                    logger.info("strict spelling")
-                    correct = arg in correct_list
-                else:
-                    logger.info("spelling leniency")
-                    correct = better_spellcheck(arg, correct_list, alias_id_list)
-
-            if correct:
-                logger.info("correct")
-
                 database.hset(f"channel:{ctx.channel.id}", "item", "")
                 database.hset(f"channel:{ctx.channel.id}", "answered", "1")
-
-                session_increment(ctx, "correct", 1)
-                streak_increment(ctx, 1)
-                database.zincrby(
-                    f"correct.user:{ctx.author.id}",
-                    1,
-                    string.capwords(str(current_item)),
-                )
-
-                await ctx.send(
-                    f"Correct! Good job! The image was **{current_item}**."
-                    if not database.exists(f"race.data:{ctx.channel.id}")
-                    else f"**{ctx.author.mention}**, you are correct! The image was **{current_item}**."
-                )
+                await ctx.send("Sorry, the image was actually " + current_item + ".")
                 url = get_wiki_url(ctx, current_item)
-                await ctx.send(
-                    url
-                    if not database.exists(f"race.data:{ctx.channel.id}")
-                    else f"<{url}>"
-                )  # sends wiki page
-                score_increment(ctx, 1)
-                if database.exists(f"race.data:{ctx.channel.id}"):
-
-                    limit = int(database.hget(f"race.data:{ctx.channel.id}", "limit"))
-                    first = database.zrevrange(
-                        f"race.scores:{ctx.channel.id}", 0, 0, True
-                    )[0]
-                    if int(first[1]) >= limit:
-                        logger.info("race ending")
-                        race = self.bot.get_cog("Race")
-                        await race.stop_race(ctx)
-                    else:
-                        logger.info("auto sending next image")
-                        group, state, bw = database.hmget(
-                            f"race.data:{ctx.channel.id}", ["group", "state", "bw"]
-                        )
-                        media = self.bot.get_cog("Media")
-                        await media.send_pic(
-                            ctx,
-                            group.decode("utf-8"),
-                            state.decode("utf-8"),
-                            bw.decode("utf-8"),
-                        )
-
-            else:
-                logger.info("incorrect")
-
-                streak_increment(ctx, None)
-                session_increment(ctx, "incorrect", 1)
-                incorrect_increment(ctx, str(current_item), 1)
-
-                if database.exists(f"race.data:{ctx.channel.id}"):
-                    await ctx.send("Sorry, that wasn't the right answer.")
-                else:
-                    database.hset(f"channel:{ctx.channel.id}", "item", "")
-                    database.hset(f"channel:{ctx.channel.id}", "answered", "1")
-                    await ctx.send(
-                        "Sorry, the image was actually " + current_item + "."
-                    )
-                    url = get_wiki_url(ctx, current_item)
-                    await ctx.send(url)
+                await ctx.send(url)
 
 
 def setup(bot):
